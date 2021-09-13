547649e34e88f49f86d44526dfad0eddb4df08c2
Christian Bohollo <cbohollo@omatech.com>
2021-09-13 21:53:57 +0200

    pagination from mcore and relations

--- src/Editora/Infrastructure/Persistence/Eloquent/Repositories/Instance/ExtractionRepository.php
+++ src/Editora/Infrastructure/Persistence/Eloquent/Repositories/Instance/ExtractionRepository.php
@@ -2,19 +2,15 @@
 
 namespace Omatech\Mapi\Editora\Infrastructure\Persistence\Eloquent\Repositories\Instance;
 
+use Illuminate\Database\Eloquent\Collection;
+use Omatech\Mapi\Editora\Infrastructure\Persistence\Eloquent\Models\InstanceDAO;
+use Omatech\Mapi\Editora\Infrastructure\Persistence\Eloquent\Models\RelationDAO;
 use Omatech\Mcore\Editora\Domain\Instance\Contracts\ExtractionRepositoryInterface;
 use Omatech\Mcore\Editora\Domain\Instance\Extraction\Pagination;
 use Omatech\Mcore\Editora\Domain\Instance\Extraction\Results;
 
 final class ExtractionRepository extends InstanceRepository implements ExtractionRepositoryInterface
 {
-    private function where(array $params)
-    {
-        return $this->instance
-            ->where('class_key', $params['class'])
-            ->orWhere('key', $params['key']);
-    }
-
     public function instancesBy(array $params): Results
     {
         $pagination = new Pagination($params, $this->where($params)->count());
@@ -26,7 +22,7 @@ final class ExtractionRepository extends InstanceRepository implements Extractio
 
     public function findChildrenInstances(int $instanceId, array $params): Results
     {
-        $pagination = new Pagination($params, $this->where($params)->count());
+        $pagination = new Pagination($params, $this->countRelations($instanceId, $params));
         $instances = $this->instance->where('id', $instanceId)
             ->with('relations', function ($q) use ($params, $pagination) {
                 $q->where('key', $params['class'])
@@ -34,13 +30,36 @@ final class ExtractionRepository extends InstanceRepository implements Extractio
                     ->with('child');
             })
             ->get();
-        $instances = $instances->reduce(function ($acc, $instance) {
-            return $acc + $instance->relations->reduce(function ($acc, $relation) {
-                $acc[] = $this->build($relation->child->class_key)
-                    ->fill($this->instanceFromDB($relation->child));
-                return $acc;
-            }, []);
+        return new Results($this->parseRelation($instances), $pagination);
+    }
+
+    private function where(array $params)
+    {
+        return $this->instance
+            ->where('class_key', $params['class'])
+            ->orWhere('key', $params['key']);
+    }
+
+    private function countRelations(int $instanceId, array $params): int
+    {
+        return $this->instance->where('id', $instanceId)
+            ->withCount(['relations' => function ($q) use ($params) {
+                $q->where('key', $params['class']);
+            },
+            ])->first()->relations_count;
+    }
+
+    private function parseRelation(Collection $instances): array
+    {
+        return $instances->reduce(function (array $acc, InstanceDAO $instance): array {
+            return $acc + $instance->relations->reduce(
+                function (array $acc, RelationDAO $relation): array {
+                    $acc[] = $this->build($relation->child->class_key)
+                        ->fill($this->instanceFromDB($relation->child));
+                    return $acc;
+                },
+                []
+            );
         }, []);
-        return new Results($instances, $pagination);
     }
 }
